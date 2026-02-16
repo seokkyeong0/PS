5485a39b657fff0901312edd2cdf6adacc6fc6cc
Seok <seokkyeong0@gmail.com>
2026-02-16 23:50:53 +0900

    [Bronze V] Title: 구구단, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    for (int i = 1; i < 10; i++) {
+        cout << n << " * " << i << " = " << n * i << '\n';
+    }
+
+    return 0;
+}
