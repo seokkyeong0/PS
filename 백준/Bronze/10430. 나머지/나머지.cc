8d1b065968234f214948844e976fc0b3450e0dae
Seok <seokkyeong0@gmail.com>
2026-02-18 18:49:36 +0900

    [Bronze V] Title: 나머지, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false); 
+    
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    cout << (a + b) % c << '\n';
+    cout << ((a % c) + (b % c)) % c << '\n';
+    cout << (a * b) % c << '\n';
+    cout << ((a % c) * (b % c)) % c;
+
+    return 0;
+}
