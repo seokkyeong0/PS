29b984aa8a4f03b5da2a2031717bd74d4672bebc
Seok <seokkyeong0@gmail.com>
2026-02-18 18:46:16 +0900

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false); 
+    
+    string s;
+    cin >> s;
+
+    cout << s << "??!";
+
+    return 0;
+}
