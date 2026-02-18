9b3fb3627d5b2d361bb1402d32e701c291bff689
Seok <seokkyeong0@gmail.com>
2026-02-18 18:47:35 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio(false); 
+    
+    int y;
+    cin >> y;
+    cout << y - 543;
+
+    return 0;
+}
