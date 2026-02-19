6823fd1120cadac66145037bb41d836233b0fcc9
Seok <seokkyeong0@gmail.com>
2026-02-19 16:22:22 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin >> t;
+    
+    for (int i = 0; i < t; i++) {
+        int a, b;
+        cin >> a >> b;
+        cout << "Case #" << i + 1 << ": " << a << " + " << b << " = " << a + b << '\n';
+    }
+    
+    return 0;
+}
