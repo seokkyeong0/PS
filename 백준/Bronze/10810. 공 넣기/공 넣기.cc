5dcfbed3192cf2701bbd50d9511b6a8fc13ce519
Seok <seokkyeong0@gmail.com>
2026-02-19 16:43:28 +0900

    [Bronze III] Title: 공 넣기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n, m;
+    cin >> n >> m;
+    
+    int a[n] = {0};
+    int b[n] = {0};
+    
+    while(m--){
+        int i, j, k;
+        cin >> i >> j >> k;
+        
+        for(int x = i; x <= j; x++){
+            a[x - 1] = k;
+        }
+    }
+    
+    for(auto i : a){
+        cout << i << ' ';
+    }
+    
+    return 0;
+}
