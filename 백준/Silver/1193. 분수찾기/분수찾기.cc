86997f41c6720807a36949879c05368dfd2a420a
Seok <seokkyeong0@gmail.com>
2026-02-21 15:07:58 +0900

    [Silver V] Title: 분수찾기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1193. 분수찾기/분수찾기.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int x;
+    cin >> x;
+
+    int n = 0, i = 1;
+    bool rev = false;
+    
+    while (x > n + i) {
+        n += i++;
+        rev ^= true;
+    }
+
+    if (x == 1)
+        cout << x << '/' << x;
+    else {
+        if (rev)
+            cout << x - n << '/' << i + 1 - (x - n);
+        else
+            cout << i + 1 - (x - n) << '/' << x - n;
+    }
+
+    return 0;
+}
