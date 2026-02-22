42509596b1f9c0e45e4cceff92b58ff966c48094
Seok <seokkyeong0@gmail.com>
2026-02-22 18:20:23 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    unsigned long long n;
+    cin >> n;
+
+    cout << n * n * n << '\n' << 3;
+
+    return 0;
+}
