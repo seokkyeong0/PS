816cf8adddb0a9285462d81429bb152550af90e7
Seok <seokkyeong0@gmail.com>
2026-02-23 20:28:55 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.cc
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.cc
@@ -2,18 +2,18 @@
 using namespace std;
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     int a, b, c, d, e, f;
     cin >> a >> b >> c >> d >> e >> f;
 
     for (int x = -999; x < 1000; x++) {
         for (int y = -999; y < 1000; y++) {
-            if ((a - d) * x + (b - e) * y == c - f) {
-                if ((a * x + b * y == c) && ((d * x) + (e * y) == f)) {
-                    cout << x << ' ' << y;
-                }
+            if (a * x + b * y == c && d * x + e * y == f) {
+                cout << x << ' ' << y;
+                return 0;
             }
         }
     }
-
-    return 0;
 }
