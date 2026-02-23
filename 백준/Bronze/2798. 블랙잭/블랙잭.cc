9aa328ba3c00dcb77dab454abf8c119b4c73bd14
Seok <seokkyeong0@gmail.com>
2026-02-23 20:38:02 +0900

    [Bronze II] Title: 블랙잭, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.cc
+++ 백준/Bronze/2798. 블랙잭/블랙잭.cc
@@ -1,34 +1,37 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
 using namespace std;
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     int n, p;
     cin >> n >> p;
-    
+
     vector<int> v;
-    vector<int> m;
-    
+
     while (n--) {
         int x;
         cin >> x;
         v.push_back(x);
     }
     
+    int max = 0;
+
     for (int i = 0; i < v.size(); i++) {
         for (int j = 0; j < v.size(); j++) {
             for (int k = 0; k < v.size(); k++) {
                 if (i != j && j != k && i != k) {
-                    if (p >= v[i] + v[j] + v[k]) {
-                        m.push_back(v[i] + v[j] + v[k]);
+                    if (p >= v[i] + v[j] + v[k] && max < v[i] + v[j] + v[k]) {
+                        max = v[i] + v[j] + v[k];
                     }
-                } 
+                }
             }
         }
     }
 
-    cout << *max_element(m.begin(), m.end());
+    cout << max;
 
     return 0;
 }
