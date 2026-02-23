2dcb10b2a1c9173dd8cc9dfa2922ba6f10b88f6f
Seok <seokkyeong0@gmail.com>
2026-02-23 21:17:49 +0900

    [Bronze II] Title: 분해합, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.cc
+++ 백준/Bronze/2231. 분해합/분해합.cc
@@ -8,48 +8,16 @@ int main() {
     int n;
     cin >> n;
 
-    int d1 = 0, d2 = 0, d3 = 0, d4 = 0;
-    int d5 = 0, d6 = 0, d7 = 0;
+    for (int i = 0; i < n; i++) {
+        int sum = i;
+        int num = i;
 
-    for (int i = 1; i < n; i++) {
-        if (i < 10) {
-            d1 = i;
-        }
-        else if (i < 100) {
-            d2 = i / 10;
-            d1 = i % 10;
-        }
-        else if (i < 1000) {
-            d3 = i / 100;
-            d2 = (i / 10) % 10;
-            d1 = i % 10;
-        }
-        else if (i < 10000) {
-            d4 = i / 1000;
-            d3 = (i / 100) % 10;
-            d2 = (i / 10) % 10;
-            d1 = i % 10;
-        }
-        else if (i < 100000) {
-            d5 = i / 10000;
-            d4 = (i / 1000) % 10;
-            d3 = (i / 100) % 10;
-            d2 = (i / 10) % 10;
-            d1 = i % 10;
-        }
-        else if (i < 1000000) {
-            d6 = i / 100000;
-            d5 = (i / 10000) % 10;
-            d4 = (i / 1000) % 10;
-            d3 = (i / 100) % 10;
-            d2 = (i / 10) % 10;
-            d1 = i % 10;
-        }
-        else {
-            d7 = 1;
+        while (num != 0) {
+            sum += num % 10;
+            num /= 10;
         }
 
-        if (i + d1 + d2 + d3 + d4 + d5 + d6 + d7 == n) {
+        if (sum == n) {
             cout << i;
             return 0;
         }
