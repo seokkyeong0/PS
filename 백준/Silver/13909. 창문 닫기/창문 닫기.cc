14406ac4b9da255c26c706e744e71323d63dab76
Seok <seokkyeong0@gmail.com>
2026-03-01 19:20:44 +0900

    [Silver V] Title: 창문 닫기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    cout << int(sqrt(n));
+    
+    return 0;
+}
