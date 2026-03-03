f71d10f6b94445935a2c4209a1f3ede71f678c33
Seok <seokkyeong0@gmail.com>
2026-03-03 22:41:25 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -1,21 +1,24 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 
-	int n;
+	int l;
 	string s;
-	cin >> n >> s;
+	cin >> l >> s;
 
-	unsigned long long h = 0;
-	for (int i = 0; i < n; i++) {
-		h += (s[i] - 96) * pow(31, i);
+	long long m = 1234567891;
+	long long r = 1;
+	long long h = 0;
+
+	for (int i = 0; i < l; i++) {
+		h += ((s[i] - 96) * r) % m;
+		r = (r * 31) % m;
 	}
 
-	cout << h % 1234567891;
+	cout << h % m;
 
 	return 0;
 }
