c9bb846eae837ed732276e4293ee01dd3ab4e4f1
Seok <seokkyeong0@gmail.com>
2026-03-07 08:52:06 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.cc
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.cc
@@ -16,17 +16,8 @@ int main() {
 
 int fibo(int n) {
     
-    // 1. 초기값 설정
-    int arr[21];
-    arr[0] = 0;
-    arr[1] = 1;
-    arr[2] = 1;
-
-    // 2. 점화식 설정
-    for (int i = 3; i <= n; i++) {
-        arr[i] = arr[i - 1] + arr[i - 2];
-    }
-
-    // 3. n번지 배열의 값을 리턴
-    return arr[n];
+    if (n == 0) return 0;
+    else if (n == 1) return 1;
+    else if (n == 2) return 1;
+    return fibo(n - 1) + fibo(n - 2);
 }
