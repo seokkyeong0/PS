f30fefe48fb1b6f17ccb30f8fc236c54a8f50173
Seok <seokkyeong0@gmail.com>
2026-03-14 12:33:18 +0900

    [Bronze V] Title: 가희와 4시간의 벽 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32775. 가희와 4시간의 벽 1/가희와 4시간의 벽 1.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int a, b;
+    cin >> a >> b;
+
+    if (a <= b) cout << "high speed rail";
+    else cout << "flight";
+
+    return 0;
+}
