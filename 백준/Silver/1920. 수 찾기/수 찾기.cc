33dcad02a2a0817c9962caf5b5fe0ff8baf6e0bc
Seok <seokkyeong0@gmail.com>
2026-03-22 00:25:30 +0900

    [Silver IV] Title: 수 찾기, Time: 44 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -1,30 +1,46 @@
 #include <iostream>
-#include <set>
+#include <algorithm>
 using namespace std;
+
+int val[100000];
+int arr[100000];
+
+int binary_search(int st, int en, int target) {
+    while (st <= en) {
+        int mid = (st + en) / 2;
+        if (arr[mid] < target)
+            st = mid + 1;
+        else if (arr[mid] > target)
+            en = mid - 1;
+        else
+            return 1;
+    }
+    return 0;
+}
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int n, m, x;
-    set<int> s;
-
+    int n;
     cin >> n;
-    while (n--) {
-        cin >> x;
-        s.insert(x);
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
     }
 
+    int m;
     cin >> m;
-    while (m--) {
-        cin >> x;
-        if (s.count(x)) {
-            cout << 1 << '\n';
-        }
-        else {
-            cout << 0 << '\n';
-        }
+
+    for (int i = 0; i < m; i++) {
+        cin >> val[i];
     }
+
+    sort(arr, arr + n);
     
+    for (int i = 0; i < m; i++) {
+        cout << binary_search(0, n - 1, val[i]) << '\n';
+    }
+
     return 0;
 }
