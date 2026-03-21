ef09c09f86e3dce5162bb548a640f1475d871ba5
Seok <seokkyeong0@gmail.com>
2026-03-22 00:41:38 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 288 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -1,29 +1,51 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
-int arr[20000001] = { 0 };
+int val[500000];
+int arr[500000];
+
+int bs_lower(int st, int en, int target) {
+    while (st < en) {
+        int mid = (st + en) / 2;
+        if (arr[mid] >= target) en = mid;
+        else st = mid + 1;
+    }
+    return st;
+}
+
+int bs_upper(int st, int en, int target) {
+    while (st < en) {
+        int mid = (st + en) / 2;
+        if (arr[mid] > target) en = mid;
+        else st = mid + 1;
+    }
+    return st;
+}
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-	int n;
-	cin >> n;
+    int n;
+    cin >> n;
 
-	while (n--) {
-		int x;
-		cin >> x;
-		arr[x + 10000000]++;
-	}
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
 
-	int m;
-	cin >> m;
+    int m;
+    cin >> m;
 
-	while (m--) {
-		int y;
-		cin >> y;
-		cout << arr[y + 10000000] << ' ';
-	}
+    for (int i = 0; i < m; i++) {
+        cin >> val[i];
+    }
 
-	return 0;
+    sort(arr, arr + n);
+    
+    for (int i = 0; i < m; i++) {
+        cout << bs_upper(0, n, val[i]) - bs_lower(0, n, val[i]) << ' ';
+    }
+
+    return 0;
 }
