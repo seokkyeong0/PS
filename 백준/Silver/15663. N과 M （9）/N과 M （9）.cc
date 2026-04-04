8836d52b1adf2a2f139321f07bc65f18ebfcca7c
Seok <seokkyeong0@gmail.com>
2026-04-05 00:43:05 +0900

    [Silver II] Title: N과 M (9), Time: 12 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
@@ -1,30 +1,31 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-#include <set>
 using namespace std;
 
 int n, m, x;
 
-set<vector<int>> s;
 vector<int> v;
 vector<int> arr(10);
 vector<int> is_used(10);
 
 void solve(int k) {
 	if (k == m) {
-		if (!s.count(arr)) {
-			s.insert(arr);
-			for (int i = 0; i < m; i++)
-				cout << arr[i] << ' ';
-			cout << '\n';
-		}
+		for (int i = 0; i < m; i++)
+			cout << arr[i] << ' ';
+		cout << '\n';
 		return;
 	}
 
+	int prev = 0;
+
 	for (int i = 0; i < n; i++) {
+		if (v[i] == prev)
+			continue;
+
 		if (!is_used[i]) {
 			arr[k] = v[i];
+			prev = v[i];
 			is_used[i] = true;
 			solve(k + 1);
 			is_used[i] = false;
