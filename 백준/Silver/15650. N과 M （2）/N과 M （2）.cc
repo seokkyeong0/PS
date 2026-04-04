26eb5060007d7c28f5c63b0959e251e358f392bc
Seok <seokkyeong0@gmail.com>
2026-04-04 23:42:56 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -7,22 +7,17 @@ bool is_used[10];
 
 void solve(int k) {
 	if (k == m) {
-		bool check = true;
-		for (int i = 1; i < m; i++) {
-			if (arr[i - 1] > arr[i]) {
-				check = false;
-			}
-		}
-			
-		if (check) {
-			for (int i = 0; i < m; i++)
-				cout << arr[i] << ' ';
-			cout << '\n';
-		}
+		for (int i = 0; i < m; i++)
+			cout << arr[i] << ' ';
+		cout << '\n';
 		return;
 	}
 
 	for (int i = 1; i <= n; i++) {
+		if (k > 0 && i <= arr[k - 1]) {
+			continue;
+		}
+
 		if (!is_used[i]) {
 			arr[k] = i;
 			is_used[i] = 1;
