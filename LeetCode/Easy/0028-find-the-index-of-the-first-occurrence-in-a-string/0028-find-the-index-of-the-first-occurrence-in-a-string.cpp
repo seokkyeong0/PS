d6a566679cfa4f8b765706b66a5d3104350a9861
Seok <seokkyeong0@gmail.com>
2026-04-22 22:12:38 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ LeetCode/Easy/0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        if (haystack == needle) return 0;
-
-        int idx_m = haystack.size();
-        int idx_n = (needle.size() > 1) ? needle.size() : 0;
 
         for (int i = 0; i <= idx_m - idx_n; i++) {
             if (haystack.substr(i, needle.size()) == needle)
