9b79cef698f1f1d9a9f93722d750143f52ce144b
Seok <seokkyeong0@gmail.com>
2026-04-26 22:04:42 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/0069-sqrtx/0069-sqrtx.cpp
+++ LeetCode/Easy/0069-sqrtx/0069-sqrtx.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        if (x == 2147483647) return 46340;
-        for (int i = 1; i < 46341; i++) {
-            if (i * i >= x) {
-                if (i * i == x) {
-                    return i;
-                } 
-                else {
-                    return i - 1;
-                }
+        if (x < 2) return x;
+        long long left = 1, right = x / 2;
+        int result = 0;
+
+        while (left <= right) {
+            long long mid = left + (right - left) / 2;
+            if (mid * mid <= x) {
+                result = mid;
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
             }
         }
-        return 0;
+        
+        return result;
     }
 };
