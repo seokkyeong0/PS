9162399200e9c0ef379e206cca9ee57cf969acdf
Seok <seokkyeong0@gmail.com>
2026-04-26 22:05:11 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/0069-sqrtx/0069-sqrtx.cpp
+++ LeetCode/Easy/0069-sqrtx/0069-sqrtx.cpp
@@ -4,7 +4,6 @@ public:
         if (x < 2) return x;
         long long left = 1, right = x / 2;
         int result = 0;
-
         while (left <= right) {
             long long mid = left + (right - left) / 2;
             if (mid * mid <= x) {
@@ -15,7 +14,6 @@ public:
                 right = mid - 1;
             }
         }
-        
         return result;
     }
 };
